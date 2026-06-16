0e89f8cd243c7b1e2744003fee5db907614081dd
Levi <1245306697@qq.com>
2026-06-16 09:29:45 +0800

    api地址

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://192.168.3.57:8000',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
